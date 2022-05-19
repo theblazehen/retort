622d92ca9550ac6dec966700dd07b3a74cfc61d4
Jiajun Du <jiajun_du@outlook.com>
2022-05-19 07:46:12 +0000

    feat: clean up bad emoji value

--- db/migrate/20220517003147_change_username_to_id.rb
+++ db/migrate/20220517003147_change_username_to_id.rb
@@ -2,6 +2,11 @@ class ChangeUsernameToId < ActiveRecord::Migration[7.0]
   def change
     details = PostDetail.where(extra: "retort")
     details.each do |detail|
+      emoji = detail.key.split("|").first
+      if !Emoji.exists?(emoji)
+        detail.destroy
+        next
+      end
       usernames = JSON.parse(detail.value)
       ids = User.where(username: usernames).ids
       detail.value = ids
